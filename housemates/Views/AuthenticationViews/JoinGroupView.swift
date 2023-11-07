6a241f799445c24945300186fb5312173a06d778
Daniel Fransesco Gunawan <dgunawan@ideate-mac-a09.local>
2023-11-07 18:35:37 -0500

    rediesigned join group view

--- housemates/Views/AuthenticationViews/JoinGroupView.swift
+++ housemates/Views/AuthenticationViews/JoinGroupView.swift
@@ -8,29 +8,67 @@
 import SwiftUI
 
 struct JoinGroupView: View {
-    @State private var group_code = ""
+    @State private var group_code: [String] = Array(repeating: "", count: 4)
+    @FocusState private var focusedField: Int?
     @EnvironmentObject var authViewModel : AuthViewModel
+
+    let numOfFields = 4
     
     var body: some View {
         VStack {
             
             
             // MARK: Housemates Title
-            Text("Enter Group Access Code!").padding(.bottom, 20)
+            Text("Enter Group Access Code!")
+                .padding(.bottom, 20)
+                .font(Font.system(size: 25))
             
             // MARK: Create a Group Form
-            VStack(spacing: 10) {
-                InputView(text: $group_code,
-                          title: "Group Code",
-                          placeholder: "8888")
-            }
+                HStack(spacing: 10) {
+                    ForEach(0..<numOfFields, id: \.self) { index in
+                        TextField("",
+                                  text: $group_code[index],
+                                  prompt: Text("0"))
+                            .keyboardType(.numberPad)
+                            .font(Font.system(size: 60))
+                            .multilineTextAlignment(.center)
+                            .padding(.all, 10)
+                            .focused($focusedField, equals: index)
+                            .onChange(of: group_code[index]) { newValue in
+                                if newValue.count >= 1 {
+                                    if newValue.count > 1 {
+                                        group_code[index] = String(newValue.prefix(1))
+                                    }
+                                    moveFocusToNextField(currentIndex: index)
+                                }
+                                else if newValue.count == 0 {
+                                    moveFocusToPrevField(currentIndex: index)
+                                }
+                                else if newValue.isEmpty {
+                                        moveFocusToPrevField(currentIndex: index)
+                                    }
+                            }
+                            .frame(width: 60, height: 80)
+                            .background(Color.white)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .overlay(RoundedRectangle(cornerRadius: 10).stroke(
+                                group_code[index].isEmpty ? Color.blue : Color.green,
+                                lineWidth: 2
+                            ))
+                    }
+                }
+                .onChange(of: focusedField) { newFocusedField in
+                    if let field = newFocusedField, group_code[field].isEmpty {
+                        focusedField = field
+                    }
+                }
         }.padding(.horizontal)
         .padding(.top, 12)
-                
+
                 // MARK: Button for Joining Existing Group
                 Button {
                     Task {
-                        try await authViewModel.joinGroup(group_code)
+                        try await authViewModel.joinGroup(group_code.joined())
                     }
                 } label: {
                     HStack {
@@ -41,9 +79,20 @@ struct JoinGroupView: View {
                     .foregroundColor(.white)
                     .frame(width: UIScreen.main.bounds.width - 32, height: 48)
                 }
-                .background(Color(.systemBlue))
+                .background(Color.blue)
                 .cornerRadius(10)
                 .padding(.top, 24)
+    }
+    func moveFocusToNextField(currentIndex: Int) {
+        if currentIndex < numOfFields - 1 {
+            focusedField = currentIndex + 1
+        }
+    }
+    
+    func moveFocusToPrevField(currentIndex: Int) {
+        if currentIndex > 0 {
+            focusedField = currentIndex - 1
+        }
     }
 }
 
