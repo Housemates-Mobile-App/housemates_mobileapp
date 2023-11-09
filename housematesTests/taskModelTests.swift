e807accaf90bb8cd24587860c7982d9f8df3d652
Daniel Fransesco Gunawan <dgunawan@ideate-mac-a09.local>
2023-11-08 21:12:37 -0500

    added initializer tests for all models

---
+++ housematesTests/taskModelTests.swift
@@ -0,0 +1,36 @@
+//
+//  taskModelTests.swift
+//  housematesTests
+//
+//  Created by Daniel Fransesco Gunawan on 11/8/23.
+//
+
+import XCTest
+@testable import housemates
+
+final class TaskModelTests: XCTestCase {
+
+    var testTask1 : task!
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        testTask1 = task(name: "clean toilet", group_id: "1", user_id: "1", description: "scrub it", status: "Pending", date_started: "10/10/2021", date_completed: "10/11/2021", priority: "high")
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        testTask1 = nil
+    }
+    
+    func testInitializeTask() {
+        XCTAssertEqual(testTask1.name, "clean toilet")
+        XCTAssertEqual(testTask1.group_id, "1")
+        XCTAssertEqual(testTask1.user_id, "1")
+        XCTAssertEqual(testTask1.description, "scrub it")
+        XCTAssertEqual(testTask1.status, "Pending")
+        XCTAssertEqual(testTask1.date_started, "10/10/2021")
+        XCTAssertEqual(testTask1.date_completed, "10/11/2021")
+        XCTAssertEqual(testTask1.priority, "high")
+    }
+
+}
---
+++ housematesTests/userModelTests.swift
@@ -0,0 +1,34 @@
+//
+//  userModelTests.swift
+//  housematesTests
+//
+//  Created by Daniel Fransesco Gunawan on 11/8/23.
+//
+
+import XCTest
+@testable import housemates
+
+final class UserModelTest: XCTestCase {
+    var testUser1 : User!
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        testUser1 = User(first_name: "Daniel", last_name: "Gunawan", is_home: false, phone_number: "123456", email: "dan@gmail.com", birthday: "10/10/2000", group_id: "2", imageURLString: "https://google.com/testImage.jpg")
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        testUser1 = nil
+    }
+    
+    func testInitializeUser() {
+        XCTAssertEqual(testUser1.first_name, "Daniel")
+        XCTAssertEqual(testUser1.last_name, "Gunawan")
+        XCTAssertEqual(testUser1.is_home, false)
+        XCTAssertEqual(testUser1.phone_number, "123456")
+        XCTAssertEqual(testUser1.email, "dan@gmail.com")
+        XCTAssertEqual(testUser1.birthday, "10/10/2000")
+        XCTAssertEqual(testUser1.group_id, "2")
+        XCTAssertEqual(testUser1.imageURLString, "https://google.com/testImage.jpg")
+    }
+}
