a33e2f0002fabc38bd545cf46a5b724e5775fd9c
Daniel Fransesco Gunawan <dgunawan@ideate-mac-a09.local>
2023-11-09 23:38:49 -0500

    changed status type

--- housematesTests/taskModelTests.swift
+++ housematesTests/taskModelTests.swift
@@ -14,7 +14,7 @@ final class TaskModelTests: XCTestCase {
     
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        testTask1 = task(name: "clean toilet", group_id: "1", user_id: "1", description: "scrub it", status: "Pending", date_started: "10/10/2021", date_completed: "10/11/2021", priority: "high")
+        testTask1 = task(name: "clean toilet", group_id: "1", user_id: "1", description: "scrub it", status: task.Status.inProgress, date_started: "10/10/2021", date_completed: "10/11/2021", priority: "high")
     }
 
     override func tearDownWithError() throws {
@@ -27,7 +27,7 @@ final class TaskModelTests: XCTestCase {
         XCTAssertEqual(testTask1.group_id, "1")
         XCTAssertEqual(testTask1.user_id, "1")
         XCTAssertEqual(testTask1.description, "scrub it")
-        XCTAssertEqual(testTask1.status, "Pending")
+        XCTAssertEqual(testTask1.status, task.Status.inProgress)
         XCTAssertEqual(testTask1.date_started, "10/10/2021")
         XCTAssertEqual(testTask1.date_completed, "10/11/2021")
         XCTAssertEqual(testTask1.priority, "high")
