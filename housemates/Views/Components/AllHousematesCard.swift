acaad4d927dfce47c054733b5a48a28b9726cb6c
Daniel Fransesco Gunawan <dgunawan@ideate-mac-a09.local>
2023-11-09 21:07:30 -0500

    added allHousematesView and connected it as appropriate

---
+++ housemates/Views/Components/AllHousematesCard.swift
@@ -0,0 +1,18 @@
+//
+//  AllHousematesCard.swift
+//  housemates
+//
+//  Created by Daniel Fransesco Gunawan on 11/9/23.
+//
+
+import SwiftUI
+
+struct AllHousematesCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AllHousematesCard()
+}
---
+++ housemates/Views/DetailViews/AllHousematesView.swift
@@ -0,0 +1,18 @@
+//
+//  AllHousematesView.swift
+//  housemates
+//
+//  Created by Daniel Fransesco Gunawan on 11/9/23.
+//
+
+import SwiftUI
+
+struct AllHousematesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AllHousematesView()
+}
