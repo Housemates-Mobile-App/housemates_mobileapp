7e025236ca4a0e6da6a5c0d96b7ef8a187596ae3
Bernie Sheng <berniesheng02@gmail.com>
2023-11-24 23:39:01 -0500

    changed frame background

--- housemates/Views/Components/TaskSelectionBox.swift
+++ housemates/Views/Components/TaskSelectionBox.swift
@@ -32,10 +32,11 @@ struct TaskSelectionBox: View {
 
     private var taskBackground: some View {
         RoundedRectangle(cornerRadius: 15)
-        .stroke(Color.black.opacity(0.25))
+//        .background(Color.black.opacity(0.25))
+        .stroke(Color.gray.opacity(0.15))
             .background(
                 RoundedRectangle(cornerRadius: 15)
-                    .fill(Color.white)
+                    .fill(Color.gray.opacity(0.15))
             )
     }
 
