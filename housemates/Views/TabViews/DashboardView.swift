62fc62d6a26b1f37197f3b3406489500adf04b52
Bernie Sheng <berniesheng02@gmail.com>
2023-12-04 00:02:45 -0500

    dashboard

--- housemates/Views/TabViews/DashboardView.swift
+++ housemates/Views/TabViews/DashboardView.swift
@@ -10,34 +10,55 @@ import SwiftUI
 struct DashboardView: View {
   @EnvironmentObject var taskViewModel: TaskViewModel
   @EnvironmentObject var authViewModel: AuthViewModel
+  @State private var selectedTab = 0
   @State var currMonth: Int = 0
-  let days: [String] = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"]
+  @State var currDay: Date = Date()
+  let days: [String] = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
     var body: some View {
       if let user = authViewModel.currentUser {
-        VStack() {
-          HStack {
+        let completed = taskViewModel.getCompletedTasksForGroup(user.group_id)
+        VStack(spacing: 0) {
+         
             Text("Dashboard")
               .font(.custom("Nunito-Bold", size: 26))
               .foregroundColor(Color(red: 0.439, green: 0.298, blue: 1.0))
+              .padding()
+      
+        
+          
+          CustomTabBar(selectedTab: $selectedTab, tabs: [(icon: "person.3.fill", title: "Statistics"), (icon: "person.fill", title: "Calendar")])
+          
+          if selectedTab == 0 {
+            
+            
+            
+            HStack {
+              Text("Your Pending Tasks")
+                .font(.custom("Lato-Bold", size: 15))
+              Spacer()
+            }.padding()
+            
+            
+            HStack {
+              Text("Statistics")
+                .font(.custom("Lato-Bold", size: 15))
+              Spacer()
+            }.padding()
+            
             Spacer()
-          }.padding()
+          }
           
-          HStack {
-            Text("Your Pending Tasks")
-              .font(.custom("Lato-Bold", size: 15))
+          if selectedTab == 1 {
+            ScrollView {
+              
+              
+              calendar(completed: completed, user: user)
+            }
             Spacer()
-          }.padding()
+          }
+         
           
-          
-          HStack {
-            Text("Statistics")
-              .font(.custom("Lato-Bold", size: 15))
-            Spacer()
-          }.padding()
-          
-          calendar()
-          
-          Spacer()
+         
         }
         
         
@@ -48,53 +69,239 @@ struct DashboardView: View {
       
         
     }
+  
+  private func getCurrMonth() -> Date {
+    let calendar = Calendar.current
+    guard let currMonth = calendar.date(byAdding: .month, value: self.currMonth, to: Date()) else {
+      return Date()
+    }
+    
+    return currMonth
+  }
   
   private func extract() -> [DateValue] {
     let calendar = Calendar.current
-    guard let currMonth = calendar.date(byAdding: .month, value: self.currMonth, to: Date()) else {
-      return []
-    }
+    let currMonth = getCurrMonth()
     
-    return currMonth.getAll().compactMap { date -> DateValue in
+    var days = currMonth.getAll().compactMap { date -> DateValue in
       let day = calendar.component(.day, from: date)
       return DateValue(day: day, date: date)
     }
+    
+    let offset = calendar.component(.weekday, from: days.first?.date ?? Date())
+    for _ in 0..<offset-1 {
+      days.insert(DateValue(day: -1, date: Date()), at: 0)
+    }
+    return days
   }
   
-  private func calendar() -> some View {
+  private func isSameDay(task: task, currDate: Date) -> Bool {
+    if let date_complete = task.date_completed {
+      let formatter = DateFormatter()
+      formatter.dateFormat = "MM.dd.yy h:mm a"
+      if let date1 = formatter.date(from: date_complete) {
+        let calendar = Calendar.current
+        return calendar.isDate(date1, inSameDayAs: currDate)
+      }
+      return false
+      
+      
+    }
+    else {
+      return false
+    }
+    
+  }
+  
+  private func SameDaySelect(calDate: Date, currDate: Date) -> Bool {
+    let calendar = Calendar.current
+      return calendar.isDate(calDate, inSameDayAs: currDate)
+      
+    
+  }
+  private func getMonthYear() -> [String] {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "MMM YYYY"
+    let date = formatter.string(from: currDay)
+    return date.components(separatedBy: " ")
+  }
+  
+  @ViewBuilder
+  func cardView(value: DateValue, completed: [task]) -> some View {
+    let deepPurple = Color(red: 0.439, green: 0.298, blue: 1.0)
+    let lightPurple = Color(red: 0.439 * 3, green: 0.298 * 3, blue: 1.0 * 3)
+   
+    VStack {
+      if value.day != -1 {
+        
+          
+          if let task = completed.first(where: { task in
+            return isSameDay(task: task, currDate: value.date)
+          }){
+            
+            VStack {
+              
+              Spacer()
+              Text("\(value.day)")
+                .font(.custom("Lato", size: 14))
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity)
+                
+              Spacer()
+            }.frame(width: 35, height: 35, alignment: .top)
+        
+              .background(
+                RoundedRectangle(cornerRadius: 5)
+                  .fill(isSameDay(task: task, currDate: value.date) ? deepPurple : .clear)
+                )
+              .overlay(
+                  RoundedRectangle(cornerRadius: 5)
+                    .stroke(SameDaySelect(calDate: value.date, currDate: currDay) ? lightPurple : .clear, lineWidth: 3)
+              )
+              
+              
+            
+          }
+          else {
+            VStack {
+              
+              
+              Spacer()
+              Text("\(value.day)")
+                .font(.custom("Lato", size: 14))
+                
+                .frame(maxWidth: .infinity)
+              Spacer()
+            }.frame(width: 35, height: 35, alignment: .top)
+            
+              .background(
+                RoundedRectangle(cornerRadius: 5)
+                  .fill(.gray.opacity(0.1))
+                )
+              .overlay(
+                  RoundedRectangle(cornerRadius: 5)
+                    .stroke(SameDaySelect(calDate: value.date, currDate: currDay) ? lightPurple : .clear, lineWidth: 3)
+              )
+//
+              
+          }
+        
+        
+      }
+    }
+    
+    
+    
+        
+    
+  }
+  
+  
+  
+  
+  private func calendar(completed: [task], user: User) -> some View {
     
 //    calendar title
     VStack {
-      HStack {
-        VStack {
-          Text("Year")
-          Text("Month")
+      
+      VStack {
+        
+        
+        HStack {
+          VStack(alignment: .leading) {
+            Text(getMonthYear()[1])
+              .font(.custom("Lato", size: 12))
+            Text(getMonthYear()[0])
+              .font(.custom("Nunito-Bold", size: 26))
+          }
+          
+          Spacer()
+          Button(action: {
+            withAnimation {
+              currMonth -= 1
+            }
+            
+            
+          }) {
+            
+            Image(systemName: "arrow.left")
+              .font(.custom("Lato-Bold", size: 15))
+            
+          }
+          Button(action: {
+            withAnimation {
+              
+              
+              currMonth += 1
+            }
+            
+          }) {
+            Image(systemName: "arrow.right")
+              .font(.custom("Lato-Bold", size: 15))
+          }
+          
+        }.padding()
+        
+        HStack {
+          ForEach(days, id: \.self) { day in
+            
+            Text(day)
+              .padding(.horizontal, 5)
+              .font(.custom("Lato-Bold", size: 12))
+              .frame(maxWidth: .infinity)
+            
+          }
         }
         
-        Spacer()
-        Button(action: {}) {
-          Text("Button")
+        let cols = Array(repeating: GridItem(.flexible()), count: 7)
+        LazyVGrid(columns: cols, spacing: 10) {
+          ForEach(extract()) { value in
+            cardView(value: value, completed: completed)
+              .padding(.vertical, 2)
+            
+            //            .background (
+            //              RoundedRectangle(cornerSize: 15, style: .continuous)
+            //              .fill(.purple)
+            //            )
+              .onTapGesture {
+                currDay = value.date
+              }
+          }
+          
         }
+      }.frame(height: 400, alignment: .top)
+      
+      
+      Text("Completed Tasks")
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .font(.custom("Lato-Bold", size: 15))
+        .padding()
+      if completed.contains(where: { task in
+          isSameDay(task: task, currDate: currDay)
+      }) {
         
+          ForEach(completed.filter({ task in
+              isSameDay(task: task, currDate: currDay)
+          }), id: \.id) { task in
+            TaskView(task: task, user: user)
+              .padding(.horizontal)
+              
+          }
+      } else {
+          Text("Nothing here, complete a task!!")
+          .frame(maxWidth: .infinity, alignment: .leading)
+          .font(.custom("Lato", size: 15))
+          .padding(.horizontal)
       }
+
       
-      HStack(spacing: 0) {
-        ForEach(days, id: \.self) { day in
-          Text(day)
-            .padding(.horizontal)
-            .font(.custom("Lato", size: 15))
-          
-        }
-      }
       
-      let cols = Array(repeating: GridItem(.flexible()), count: 7)
-      LazyVGrid(columns: cols, spacing: 15) {
-        ForEach(extract()) { value in
-          Text("\(value.day)")
-            .font(.custom("Lato", size: 12))
-          
-        }
-      }
+    }
+    .padding()
+    
+    .onChange(of: currMonth) { newVal in
+      currDay = getCurrMonth()
+      
     }
     
     
@@ -102,6 +309,42 @@ struct DashboardView: View {
   }
 }
 
+struct CustomTabBar: View {
+    @Binding var selectedTab: Int
+    let tabs: [(icon: String, title: String)]
+    @Namespace private var namespace
+    let deepPurple = Color(red: 0.439, green: 0.298, blue: 1.0)
+    var body: some View {
+        HStack {
+            ForEach(0..<tabs.count, id: \.self) { index in
+                Button(action: {
+                    withAnimation(.easeInOut) {
+                        self.selectedTab = index
+                    }
+                }) {
+                    VStack {
+                      HStack {
+                        Image(systemName: tabs[index].icon)
+                        Text(tabs[index].title)
+                          .font(.custom("Nunito-Bold", size: 15))
+                      }
+                     
+                        if selectedTab == index {
+                            deepPurple.frame(height: 2)
+                                .matchedGeometryEffect(id: "tabIndicator", in: namespace)
+                        } else {
+                            Color.clear.frame(height: 2)
+                        }
+                    }
+                }
+                .foregroundColor(self.selectedTab == index ? deepPurple : .gray)
+            }
+        }
+        .padding([.horizontal])
+        .background(Color.white)
+        
+    }
+}
 struct DashboardView_Previews: PreviewProvider {
     static var previews: some View {
         DashboardView()
@@ -111,9 +354,11 @@ struct DashboardView_Previews: PreviewProvider {
 extension Date {
   func getAll() -> [Date] {
     let calendar = Calendar.current
+    let start = calendar.date(from: Calendar.current.dateComponents([.year, .month], from: self))!
     let range = calendar.range(of: .day, in: .month, for: self)!
+  
     return range.compactMap { day -> Date in
-      return calendar.date(byAdding: .day, value: day, to: self)!
+      return calendar.date(byAdding: .day, value: day - 1, to: start)!
     }
   }
 }
