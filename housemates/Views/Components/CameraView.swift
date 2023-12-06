bfdef548948ff28b8534bc878e33aa6b45726bf2
Sanmoy Karmakar <moy@sanmoys-air.mynetworksettings.com>
2023-12-05 19:25:02 -0500

    Created Custom CameraView when claiming a task with skip, retake, and back button functionality

--- housemates/Views/Components/CameraView.swift
+++ housemates/Views/Components/CameraView.swift
@@ -6,52 +6,117 @@
 //
 
 import SwiftUI
-import UIKit
+import AVFoundation
 
-struct CameraView: UIViewControllerRepresentable {
-    @Environment(\.presentationMode) var presentationMode
-    @Binding var image: UIImage?
-    @Binding var isShown: Bool
+// MARK: - Custom Camera View Controller
+class CustomCameraViewController: UIViewController {
+    var captureSession: AVCaptureSession!
+    var photoOutput: AVCapturePhotoOutput!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    
+    var onPhotoCapture: ((UIImage?) -> Void)?
 
-    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-        let parent: CameraView
-
-        init(_ parent: CameraView) {
-            self.parent = parent
-        }
-
-        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
-            if let image = info[.originalImage] as? UIImage {
-                parent.image = image
-            } 
-//            else {
-//                parent.image = UIImage(named: "default")!
-//            }
-
-            parent.isShown = false
-        }
-
-        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-            parent.isShown = false
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCaptureSession()
+    }
+    
+    func setupCaptureSession() {
+        captureSession = AVCaptureSession()
+        captureSession.beginConfiguration()
+        
+        // Add video input.
+        guard let videoDevice = AVCaptureDevice.default(for: .video),
+              let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice),
+              captureSession.canAddInput(videoDeviceInput) else { return }
+        
+        captureSession.addInput(videoDeviceInput)
+        
+        // Add photo output.
+        photoOutput = AVCapturePhotoOutput()
+        guard captureSession.canAddOutput(photoOutput) else { return }
+        captureSession.sessionPreset = .photo
+        captureSession.addOutput(photoOutput)
+        
+        captureSession.commitConfiguration()
+        
+        // Add preview layer.
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        previewLayer.frame = view.bounds
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if !captureSession.isRunning {
+            DispatchQueue.global(qos: .userInitiated).async {
+                self.captureSession.startRunning()
+            }
         }
     }
-
-    func makeCoordinator() -> Coordinator {
-        return Coordinator(self)
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if captureSession.isRunning {
+            DispatchQueue.global(qos: .userInitiated).async {
+                self.captureSession.stopRunning()
+            }
+        }
     }
-
-    func makeUIViewController(context: UIViewControllerRepresentableContext<CameraView>) -> UIImagePickerController {
-        let picker = UIImagePickerController()
-        picker.delegate = context.coordinator
-        picker.sourceType = .camera
-        return picker
-    }
-
-    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<CameraView>) {
-        // No update required
+    
+    func captureImage() {
+        guard let photoOutput = self.photoOutput else {
+            print("Error: photoOutput is nil")
+            return
+        }
+        let settings = AVCapturePhotoSettings()
+        photoOutput.capturePhoto(with: settings, delegate: self)
     }
 }
 
-//#Preview {
-//    CameraView()
-//}
+// MARK: - AVCapturePhotoCaptureDelegate
+extension CustomCameraViewController: AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        guard let imageData = photo.fileDataRepresentation(),
+              let image = UIImage(data: imageData) else {
+            onPhotoCapture?(nil)
+            return
+        }
+        onPhotoCapture?(image)
+    }
+}
+
+// MARK: - UIViewControllerRepresentable for Custom Camera
+struct CustomCameraViewRepresentable: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    @Binding var isPresented: Bool
+    @Binding var takePhoto: Bool  // A binding to control when to take a photo
+
+    func makeUIViewController(context: Context) -> CustomCameraViewController {
+        let controller = CustomCameraViewController()
+        controller.onPhotoCapture = { [self] image in
+            DispatchQueue.main.async {
+                self.image = image
+                // Only show the preview if an image was captured
+                if image != nil {
+                    self.isPresented = true
+                }
+            }
+        }
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: CustomCameraViewController, context: Context) {
+        if takePhoto {
+            uiViewController.captureImage()
+//            DispatchQueue.main.async {
+//                self.takePhoto = false  // Reset the state after taking the photo
+//            }
+        }
+    }
+}
--- housemates/Views/DetailViews/TaskView.swift
+++ housemates/Views/DetailViews/TaskView.swift
@@ -228,8 +228,21 @@ struct TaskView: View {
             }) {
                 Text("DONE")
             }
-            .sheet(isPresented: $showCamera) {
-                CameraView(image: $capturedImage, isShown: $showCamera)
+            .fullScreenCover(isPresented: $showCamera) {
+                BeforeCameraView(image: $capturedImage, isPresented: $showCamera, onClaimTask: { takenImage in
+                    if let image = takenImage {
+                        // Claim the task with the image
+                        Task {
+                            await taskViewModel.claimTask(task: task, user_id: user.id ?? "", image: image)
+                        }
+                    } else {
+                        // Claim the task without the image
+                        Task {
+                            await taskViewModel.claimTask(task: task, user_id: user.id ?? "", image: nil)
+                        }
+                    }
+                }
+                )
             }
             .onChange(of: capturedImage) { _ in
                 if let _ = capturedImage {
@@ -251,30 +264,40 @@ struct TaskView: View {
     // MARK: - Claim Button
     private var claimButton: some View {
         Button("CLAIM", action: {
-            
           self.showCamera = true
-          
 //          DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
 //            if let uid = user.id {
 //                taskViewModel.claimTask(task: task, user_id: uid)
 //           
 //            }
 //          }
-            
         })
         .buttonStyle(ClaimButtonStyle())
         .fullScreenCover(isPresented: $showCamera) {
-            CustomCameraView(image: $capturedImage, isPresented: $showCamera)
-        }
-        .onChange(of: capturedImage) { _ in
-            if let image = capturedImage {
-                  if let uid = user.id {
-                      Task {
-                          await taskViewModel.claimTask(task: task, user_id: uid, image: image)
-                  }
+            BeforeCameraView(image: $capturedImage, isPresented: $showCamera, onClaimTask: { takenImage in
+                if let image = takenImage {
+                    // Claim the task with the image
+                    Task {
+                        await taskViewModel.claimTask(task: task, user_id: user.id ?? "", image: image)
+                    }
+                } else {
+                    // Claim the task without the image
+                    Task {
+                        await taskViewModel.claimTask(task: task, user_id: user.id ?? "", image: nil)
+                    }
                 }
             }
+            )
         }
+//        .onChange(of: capturedImage) { _ in
+//            if let image = capturedImage {
+//                  if let uid = user.id {
+//                      Task {
+//                          await taskViewModel.claimTask(task: task, user_id: uid, image: image)
+//                  }
+//                }
+//            }
+//        }
     }
 
     // MARK: - User Profile Image
